e1290c1da04fc6471b965cb2df34c1bb041f9330
AleksandrMelexin <Melehin-Sasha7@yandex.ru>
2024-03-26 16:30:41 +0300

    работа 6 исправлена

--- lab6/main.py
+++ lab6/main.py
@@ -1,38 +1,27 @@
 import tkinter as tk
-import lib
-
-def calculate():
-    a = float(entry_a.get())
-    b = float(entry_b.get())
-    c = float(entry_c.get())
-    result = lib.uravnenie(a, b, c)
-    result_label.config(text=result)
+import ctypes
+import os
 
 
-root = tk.Tk()
-root.title("lab6")
-root.geometry("270x160")
-root.resizable(False, False)
+def call_dll_function():
+    # Загружаем dll файл
+    os.add_dll_directory(os.getcwd())
+    my_dll = ctypes.CDLL('my_dll.dll')
 
-label_a = tk.Label(root, text="Коэффициент a:")
-label_a.grid(row=0, column=0, padx=5, pady=5)
-entry_a = tk.Entry(root)
-entry_a.grid(row=0, column=1, padx=5, pady=5)
+    # Вызываем функцию из dll файла
+    new_text = my_dll.my_function()
+    label.config(text=new_text)
 
-label_b = tk.Label(root, text="Коэффициент b:")
-label_b.grid(row=1, column=0, padx=5, pady=5)
-entry_b = tk.Entry(root)
-entry_b.grid(row=1, column=1, padx=5, pady=5)
 
-label_c = tk.Label(root, text="Коэффициент c:")
-label_c.grid(row=2, column=0, padx=5, pady=5)
-entry_c = tk.Entry(root)
-entry_c.grid(row=2, column=1, padx=5, pady=5)
+app = tk.Tk()
+app.title("lab6")
+app.geometry("200x200")
+app.resizable(False, False)
 
-calculate_button = tk.Button(root, text="Вычислить", command=calculate)
-calculate_button.grid(row=3, columnspan=2, padx=5, pady=5)
+button = tk.Button(app, text="Call DLL Function", command=call_dll_function)
+button.pack()
 
-result_label = tk.Label(root, text="")
-result_label.grid(row=4, columnspan=2, padx=5, pady=5)
+label = tk.Label(app, text="")
+label.pack()
 
-root.mainloop()
+app.mainloop()
